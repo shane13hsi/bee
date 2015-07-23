15b58537088872fa4245d6a0fdd1e93c50d4e172
Shane Hsi <shane13hsi@gmail.com>
2015-07-23 16:03:57 +0800

    Karma Jasmine

---
+++ karma.conf.js
@@ -0,0 +1,27 @@
+module.exports = function(config) {
+  config.set({
+    browsers: ['Chrome'],
+    files: [
+      {
+        pattern: 'test-context.js', watched: false   // 无需 watch 此类文件
+      }
+    ],
+    frameworks: ['jasmine'],
+    preprocessors: {
+      'test-context.js': ['webpack']
+    },
+    reporters: ['dots'],
+    singleRun: true,
+    webpack: {
+      module: {
+        loaders: [
+          {test: /\.js?$/, exclude: /node_modules/, loader: 'babel-loader'}
+        ]
+      },
+      watch: true
+    },
+    webpackServer: {
+      noInfo: true
+    }
+  });
+};
--- test/index.spec.js
+++
@@ -1,8 +0,0 @@
-import {assert} from 'chai';
-import { add } from '../src';
-
-describe('add', () => {
-  it('should add 2 and 2', () => {
-    assert(add(2, 2) === 4);
-});
-});
