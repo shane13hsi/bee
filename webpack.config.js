0aefcb74d27503d297b479d2bcec2b206e3a75bd
Shane Hsi <shane13hsi@gmail.com>
2015-07-24 14:25:59 +0800

    Add hot load

--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,30 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: {
-    app: ['./src/main.js'],
+    app: [
+      'webpack-dev-server/client?http://0.0.0.0:8080',
+      'webpack/hot/only-dev-server',
+      './src/main.js'
+    ],
     vendors: ['react']
   },
 
   module: {
     loaders: [
-      {test: /\.js$/, include: constants.SRC_DIR, loader: 'babel-loader'}
+      {test: /\.js$/, include: constants.SRC_DIR, loaders: ['react-hot', 'babel-loader']}
     ],
     noParse: [path.join(constants.NODE_MODULES_DIR, 'react/dist/react-with-addons.min.js')]
   },
 
   output: {
-    path: './build',
-    filename: 'bundle.js'
+    path: constants.BUILD_DIR,
+    filename: 'bundle.js',
+    publicPath: '/static/'
   },
 
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
     new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
   ],
 
