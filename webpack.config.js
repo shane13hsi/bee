9143e70d2ad28e69ee935a7a69f87ae0a2e59ddd
Shane Hsi <shane13hsi@gmail.com>
2015-07-24 11:15:44 +0800

    noParse

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var constants = require('./constants');
+var path = require('path');
 
 module.exports = {
   entry: ['./src/main.js'],
@@ -6,7 +7,16 @@ module.exports = {
     path: './build',
     filename: 'bundle.js'
   },
+
+  resolve: {
+    alias: {
+      'react$': path.join(constants.NODE_MODULES_DIR, 'react/dist/react-with-addons.min.js')
+    },
+    extensions: ['', '.js', '.jsx', '.json']
+  },
+
   module: {
+    noParse: [path.join(constants.NODE_MODULES_DIR, 'react/dist/react-with-addons.min.js')],
     loaders: [
       {test: /\.js$/, include: constants.SRC_DIR, loader: 'babel-loader'}
     ]
