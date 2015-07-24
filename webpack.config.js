2be304696320a33e838351a6786b585d14c4556e
Shane Hsi <shane13hsi@gmail.com>
2015-07-24 11:20:16 +0800

    Specify chunks, common chunks

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,33 @@
 var constants = require('./constants');
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-  entry: ['./src/main.js'],
+  entry: {
+    app: ['./src/main.js'],
+    vendors: ['react']
+  },
+
+  module: {
+    loaders: [
+      {test: /\.js$/, include: constants.SRC_DIR, loader: 'babel-loader'}
+    ],
+    noParse: [path.join(constants.NODE_MODULES_DIR, 'react/dist/react-with-addons.min.js')]
+  },
+
   output: {
     path: './build',
     filename: 'bundle.js'
   },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+  ],
 
   resolve: {
     alias: {
       'react$': path.join(constants.NODE_MODULES_DIR, 'react/dist/react-with-addons.min.js')
     },
     extensions: ['', '.js', '.jsx', '.json']
-  },
-
-  module: {
-    noParse: [path.join(constants.NODE_MODULES_DIR, 'react/dist/react-with-addons.min.js')],
-    loaders: [
-      {test: /\.js$/, include: constants.SRC_DIR, loader: 'babel-loader'}
-    ]
   }
 };
