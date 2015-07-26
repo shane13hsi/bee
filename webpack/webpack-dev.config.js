bd0a1fbc610da38203d876ac9f0f08e6f8cfd3f2
Shane Hsi <shane13hsi@gmail.com>
2015-07-26 23:28:53 +0800

    Optimize webpack-production.config. Add to npm scripts

--- webpack/webpack-dev.config.js
+++ webpack/webpack-dev.config.js
@@ -28,6 +28,11 @@ module.exports = {
   },
 
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('development')
+      }
+    }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
--- webpack/webpack-production.config.js
+++ webpack/webpack-production.config.js
@@ -1,10 +1,11 @@
 var path = require('path');
 var webpack = require('webpack');
 var constants = require('../constants');
+var NyanProgressPlugin = require('nyan-progress-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: ['./src/main.js'],
+    app: path.join(constants.SRC_DIR, 'main.js'),
     vendors: ['react']
   },
 
@@ -17,11 +18,37 @@ module.exports = {
 
   output: {
     path: constants.DIST_DIR,
-    filename: 'bundle.js'
+    filename: '[name].js',
+    chunkFilename: '[name]-[chunkhash].js'
   },
 
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new NyanProgressPlugin(),
+    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      // keep_fnames prevents function name mangling.
+      // Function names are useful. Seeing a readable error stack while
+      // being able to programmatically analyse it is priceless. And yes,
+      // we don't need infamous FLUX_ACTION_CONSTANTS with function name.
+      // It's ES6 standard polyfilled by Babel.
+      /* eslint-disable camelcase */
+      compress: {
+        keep_fnames: true,
+        screw_ie8: true,
+        warnings: false // Because uglify reports irrelevant warnings.
+      },
+      mangle: {
+        keep_fnames: true
+      }
+      /* eslint-enable camelcase */
+    })
   ],
 
   resolve: {
