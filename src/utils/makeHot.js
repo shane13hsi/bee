202eb027b8c31fab0d6850e16ad932a1f30b34e1
Shane Hsi <shane13hsi@gmail.com>
2015-07-29 10:05:19 +0800

    Add docs

--- src/utils/makeHot.js
+++ src/utils/makeHot.js
@@ -1,3 +1,9 @@
+/**
+ * @see https://github.com/goatslacker/alt/issues/29
+ * @see https://github.com/goatslacker/alt/issues/85
+ *
+ * @note 另外，提供的 util  方法，module.hot.dispose 无法运行
+ * */
 function makeHot(alt, Store) {
   let name = arguments[2] === undefined ? Store.displayName : arguments[2];
 
