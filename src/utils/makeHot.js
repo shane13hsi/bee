28dceadd4d021477c34adaff6d587521d072f426
Shane Hsi <shane13hsi@gmail.com>
2015-07-29 11:50:32 +0800

    Update doc

--- src/utils/makeHot.js
+++ src/utils/makeHot.js
@@ -2,7 +2,7 @@
  * @see https://github.com/goatslacker/alt/issues/29
  * @see https://github.com/goatslacker/alt/issues/85
  *
- * @note 另外，提供的 util  方法，module.hot.dispose 无法运行
+ * @note alt/utils/makeHot will not run dispose cb
  * */
 function makeHot(alt, Store) {
   let name = arguments[2] === undefined ? Store.displayName : arguments[2];
