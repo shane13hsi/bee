821ba95b945728f65dd9351e68e781b30f827514
Shane Hsi <shane13hsi@gmail.com>
2015-09-06 18:17:11 +0800

    Introduce redux-devtools

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route } from 'react-router';
 import createHashHistory from 'history/lib/createHashHistory';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
 import configureStore from './store/configureStore';
 import App from './containers/App';
@@ -11,12 +12,17 @@ const history = createHashHistory();
 const store = configureStore();
 
 React.render(
-  <Provider store={store}>
-    {() =>
-      <Router history={history}>
-        <Route component={App} path='/'/>
-      </Router>
-    }
-  </Provider>,
+  <div>
+    <Provider store={store}>
+      {() =>
+        <Router history={history}>
+          <Route component={App} path='/'/>
+        </Router>
+      }
+    </Provider>
+    <DebugPanel top right bottom>
+      <DevTools store={store} monitor={LogMonitor}/>
+    </DebugPanel>
+  </div>,
   document.getElementById('root')
 );
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,5 @@
-import { createStore, applyMiddleware } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
+import { devTools, persistState } from 'redux-devtools';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import reducer from '../reducers';
@@ -8,12 +9,14 @@ const logger = createLogger({
   collapsed: true
 });
 
-const createStoreWithMiddleware = applyMiddleware(
-  thunk, logger
+const finalCreateStore = compose(
+  applyMiddleware(thunk, logger),
+  devTools(),
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
 )(createStore);
 
 export default function configureStore(initialState) {
-  const store = createStoreWithMiddleware(reducer, initialState);
+  const store = finalCreateStore(reducer, initialState);
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
